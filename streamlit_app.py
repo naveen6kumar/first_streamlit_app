6ac55fadc2c6bb82844c538e5d00835d201b53df
naveen6kumar <141913344+naveen6kumar@users.noreply.github.com>
2023-08-10 16:36:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,10 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast menu')
 
 streamlit.write('Naveen is here')
-
 streamlit.text('This is a text line Boss 🥗')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
 '''
-pandas.read_csv()
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
