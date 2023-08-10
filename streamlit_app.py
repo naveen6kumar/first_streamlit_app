436c6143a19459fa5771b79d233b26a813989618
naveen6kumar <141913344+naveen6kumar@users.noreply.github.com>
2023-08-10 16:39:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 import pandas 
-"""
-streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast menu')
 
+streamlit.title('My Parents New Healthy Diner')
+"""
+streamlit.title('Breakfast menu')
 streamlit.write('Naveen is here')
 streamlit.text('This is a text line Boss 🥗')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
